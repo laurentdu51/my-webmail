9e16b7da2a298999cf5c2a0c1c88e0a2c3cf9100
MrDuhaz <laurent@duhaz.fr>
2025-10-21 13:45:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['rpidob', 'webmail.duhaz.fr'],
+    // other server options like port, host, etc.
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
